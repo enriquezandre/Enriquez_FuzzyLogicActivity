8714ca15516e72b40998e95eca0464e6b688d2ba
Andre Enriquez <andre.enriquez@cit.edu>
2022-12-16 04:52:31 +0800

    Implement project

--- EnriquezFuzzyLogic/EnriquezFuzzyLogic/Form1.Designer.cs
+++ EnriquezFuzzyLogic/EnriquezFuzzyLogic/Form1.Designer.cs
@@ -28,13 +28,192 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.tempChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.heatChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.ONButton = new System.Windows.Forms.Button();
+            this.waterLevelLabel = new System.Windows.Forms.Label();
+            this.OFFButton = new System.Windows.Forms.Button();
+            this.RESETButton = new System.Windows.Forms.Button();
+            this.waterlevelTextBox = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.heatknobDataLabel = new System.Windows.Forms.Label();
+            this.tempDataLabel = new System.Windows.Forms.Label();
+            this.waterlevelDataLabel = new System.Windows.Forms.Label();
+            this.fuzzyLogicLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.tempChart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.heatChart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tempChart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.tempChart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.tempChart.Legends.Add(legend1);
+            this.tempChart.Location = new System.Drawing.Point(0, 0);
+            this.tempChart.Name = "tempChart";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.tempChart.Series.Add(series1);
+            this.tempChart.Size = new System.Drawing.Size(397, 300);
+            this.tempChart.TabIndex = 0;
+            this.tempChart.Text = "chart1";
+            // 
+            // heatChart
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.heatChart.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.heatChart.Legends.Add(legend2);
+            this.heatChart.Location = new System.Drawing.Point(436, 0);
+            this.heatChart.Name = "heatChart";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.heatChart.Series.Add(series2);
+            this.heatChart.Size = new System.Drawing.Size(397, 300);
+            this.heatChart.TabIndex = 1;
+            this.heatChart.Text = "chart2";
+            // 
+            // ONButton
+            // 
+            this.ONButton.Enabled = false;
+            this.ONButton.Location = new System.Drawing.Point(262, 343);
+            this.ONButton.Name = "ONButton";
+            this.ONButton.Size = new System.Drawing.Size(75, 23);
+            this.ONButton.TabIndex = 4;
+            this.ONButton.Text = "ON";
+            this.ONButton.UseVisualStyleBackColor = true;
+            this.ONButton.Click += new System.EventHandler(this.ONButton_Click);
+            // 
+            // waterLevelLabel
+            // 
+            this.waterLevelLabel.AutoSize = true;
+            this.waterLevelLabel.Location = new System.Drawing.Point(37, 345);
+            this.waterLevelLabel.Name = "waterLevelLabel";
+            this.waterLevelLabel.Size = new System.Drawing.Size(68, 13);
+            this.waterLevelLabel.TabIndex = 3;
+            this.waterLevelLabel.Text = "Water Level:";
+            // 
+            // OFFButton
+            // 
+            this.OFFButton.Enabled = false;
+            this.OFFButton.Location = new System.Drawing.Point(343, 345);
+            this.OFFButton.Name = "OFFButton";
+            this.OFFButton.Size = new System.Drawing.Size(75, 23);
+            this.OFFButton.TabIndex = 5;
+            this.OFFButton.Text = "OFF";
+            this.OFFButton.UseVisualStyleBackColor = true;
+            this.OFFButton.Click += new System.EventHandler(this.OFFButton_Click);
+            // 
+            // RESETButton
+            // 
+            this.RESETButton.Location = new System.Drawing.Point(424, 345);
+            this.RESETButton.Name = "RESETButton";
+            this.RESETButton.Size = new System.Drawing.Size(75, 23);
+            this.RESETButton.TabIndex = 5;
+            this.RESETButton.Text = "RESET";
+            this.RESETButton.UseVisualStyleBackColor = true;
+            this.RESETButton.Click += new System.EventHandler(this.RESETButton_Click);
+            // 
+            // waterlevelTextBox
+            // 
+            this.waterlevelTextBox.Location = new System.Drawing.Point(157, 345);
+            this.waterlevelTextBox.Name = "waterlevelTextBox";
+            this.waterlevelTextBox.Size = new System.Drawing.Size(99, 20);
+            this.waterlevelTextBox.TabIndex = 2;
+            this.waterlevelTextBox.KeyUp += new System.Windows.Forms.KeyEventHandler(this.waterlevelTextBox_KeyUp);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(749, 396);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(8, 8);
+            this.panel1.TabIndex = 1;
+            // 
+            // heatknobDataLabel
+            // 
+            this.heatknobDataLabel.AutoSize = true;
+            this.heatknobDataLabel.Location = new System.Drawing.Point(333, 391);
+            this.heatknobDataLabel.Name = "heatknobDataLabel";
+            this.heatknobDataLabel.Size = new System.Drawing.Size(61, 13);
+            this.heatknobDataLabel.TabIndex = 0;
+            this.heatknobDataLabel.Text = "Heat Knob:";
+            // 
+            // tempDataLabel
+            // 
+            this.tempDataLabel.AutoSize = true;
+            this.tempDataLabel.Location = new System.Drawing.Point(529, 391);
+            this.tempDataLabel.Name = "tempDataLabel";
+            this.tempDataLabel.Size = new System.Drawing.Size(70, 13);
+            this.tempDataLabel.TabIndex = 0;
+            this.tempDataLabel.Text = "Temperature:";
+            // 
+            // waterlevelDataLabel
+            // 
+            this.waterlevelDataLabel.AutoSize = true;
+            this.waterlevelDataLabel.Location = new System.Drawing.Point(154, 391);
+            this.waterlevelDataLabel.Name = "waterlevelDataLabel";
+            this.waterlevelDataLabel.Size = new System.Drawing.Size(36, 13);
+            this.waterlevelDataLabel.TabIndex = 0;
+            this.waterlevelDataLabel.Text = "Level:";
+            // 
+            // fuzzyLogicLabel
+            // 
+            this.fuzzyLogicLabel.AutoSize = true;
+            this.fuzzyLogicLabel.Location = new System.Drawing.Point(37, 391);
+            this.fuzzyLogicLabel.Name = "fuzzyLogicLabel";
+            this.fuzzyLogicLabel.Size = new System.Drawing.Size(92, 13);
+            this.fuzzyLogicLabel.TabIndex = 0;
+            this.fuzzyLogicLabel.Text = "Fuzzy Logic Data:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(833, 456);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.tempDataLabel);
+            this.Controls.Add(this.heatknobDataLabel);
+            this.Controls.Add(this.waterlevelDataLabel);
+            this.Controls.Add(this.RESETButton);
+            this.Controls.Add(this.fuzzyLogicLabel);
+            this.Controls.Add(this.waterlevelTextBox);
+            this.Controls.Add(this.OFFButton);
+            this.Controls.Add(this.ONButton);
+            this.Controls.Add(this.heatChart);
+            this.Controls.Add(this.tempChart);
+            this.Controls.Add(this.waterLevelLabel);
+            this.Name = "Form1";
+            this.Text = "Enriquez Fuzzy Logic Activity";
+            ((System.ComponentModel.ISupportInitialize)(this.tempChart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.heatChart)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart tempChart;
+        private System.Windows.Forms.DataVisualization.Charting.Chart heatChart;
+        private System.Windows.Forms.Button ONButton;
+        private System.Windows.Forms.Label waterLevelLabel;
+        private System.Windows.Forms.Button OFFButton;
+        private System.Windows.Forms.Button RESETButton;
+        private System.Windows.Forms.TextBox waterlevelTextBox;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label heatknobDataLabel;
+        private System.Windows.Forms.Label tempDataLabel;
+        private System.Windows.Forms.Label waterlevelDataLabel;
+        private System.Windows.Forms.Label fuzzyLogicLabel;
     }
 }
 
--- EnriquezFuzzyLogic/EnriquezFuzzyLogic/Form1.cs
+++ EnriquezFuzzyLogic/EnriquezFuzzyLogic/Form1.cs
@@ -1,20 +1,254 @@
-﻿using System;
+﻿using DotFuzzy;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace EnriquezFuzzyLogic
 {
     public partial class Form1 : Form
     {
+        FuzzyEngine fe;
+        MembershipFunctionCollection waterlevel, temp, heatknob;
+        LinguisticVariable mywaterlevel, mytemp, myheatknob;
+        FuzzyRuleCollection myrules;
+
+        double water, heat, temperature;
+        double targetTemperature = 100;
+        bool exit = false;
+
         public Form1()
         {
             InitializeComponent();
+            setMembers();
+            setRules();
+            setFuzzyEngine();
+            setCharts();
+        }
+
+        public void setMembers()
+        {
+            waterlevel = new MembershipFunctionCollection();
+            waterlevel.Add(new MembershipFunction("LOW", 1.0, 2.0, 2.0, 4.0));
+            waterlevel.Add(new MembershipFunction("OK", 3.5, 5.5, 5.5, 7.0));
+            waterlevel.Add(new MembershipFunction("HIGH", 6.5, 8.0, 8.0, 9.5));
+            mywaterlevel = new LinguisticVariable("WATER_LEVEL", waterlevel);
+
+            temp = new MembershipFunctionCollection();
+            temp.Add(new MembershipFunction("VLOW", -1.0, 10.5, 10.5, 20.0));
+            temp.Add(new MembershipFunction("LOW", 10.5, 25.5, 25.5, 40.0));
+            temp.Add(new MembershipFunction("OK", 30.0, 50.5, 50.5, 80.0));
+            temp.Add(new MembershipFunction("HIGH", 60.0, 85.0, 85.0, 95.5));
+            mytemp = new LinguisticVariable("TEMPERATURE", temp);
+
+            
+            heatknob = new MembershipFunctionCollection();
+            heatknob.Add(new MembershipFunction("V_LITTLE", -1.0, 1.0, 1.0, 2.0));
+            heatknob.Add(new MembershipFunction("A_LITTLE", 1.5, 2.5, 2.5, 4.0));
+            heatknob.Add(new MembershipFunction("A_GOOD_AMT", 3.5, 5.0, 5.0, 7.0));
+            heatknob.Add(new MembershipFunction("A_LOT", 6.5, 7.0, 7.0, 8.5));
+            heatknob.Add(new MembershipFunction("A_WHOLE_LOT", 7.5, 9.5, 9.5, 11.5));
+            myheatknob = new LinguisticVariable("HEAT_KNOB", heatknob);
+        }
+
+        public void setRules()
+        {
+            myrules = new FuzzyRuleCollection();
+
+            // LOW
+            myrules.Add(new FuzzyRule("IF (WATER_LEVEL IS LOW) AND (TEMPERATURE IS VLOW) THEN HEAT_KNOB IS A_LOT"));
+            myrules.Add(new FuzzyRule("IF (WATER_LEVEL IS LOW) AND (TEMPERATURE IS LOW) THEN HEAT_KNOB IS A_GOOD_AMT"));
+            myrules.Add(new FuzzyRule("IF (WATER_LEVEL IS LOW) AND (TEMPERATURE IS OK) THEN HEAT_KNOB IS V_LITTLE"));
+            myrules.Add(new FuzzyRule("IF (WATER_LEVEL IS LOW) AND (TEMPERATURE IS HIGH) THEN HEAT_KNOB IS V_LITTLE"));
+
+            // OK
+            myrules.Add(new FuzzyRule("IF (WATER_LEVEL IS OK) AND (TEMPERATURE IS VLOW) THEN HEAT_KNOB IS A_WHOLE_LOT"));
+            myrules.Add(new FuzzyRule("IF (WATER_LEVEL IS OK) AND (TEMPERATURE IS LOW) THEN HEAT_KNOB IS A_LOT"));
+            myrules.Add(new FuzzyRule("IF (WATER_LEVEL IS OK) AND (TEMPERATURE IS OK) THEN HEAT_KNOB IS A_GOOD_AMT"));
+            myrules.Add(new FuzzyRule("IF (WATER_LEVEL IS OK) AND (TEMPERATURE IS HIGH) THEN HEAT_KNOB IS V_LITTLE"));
+
+            // HIGH
+            myrules.Add(new FuzzyRule("IF (WATER_LEVEL IS HIGH) AND (TEMPERATURE IS VLOW) THEN HEAT_KNOB IS A_WHOLE_LOT"));
+            myrules.Add(new FuzzyRule("IF (WATER_LEVEL IS HIGH) AND (TEMPERATURE IS LOW) THEN HEAT_KNOB IS A_LOT"));
+            myrules.Add(new FuzzyRule("IF (WATER_LEVEL IS HIGH) AND (TEMPERATURE IS OK) THEN HEAT_KNOB IS A_LOT"));
+            myrules.Add(new FuzzyRule("IF (WATER_LEVEL IS HIGH) AND (TEMPERATURE IS HIGH) THEN HEAT_KNOB IS A_LITTLE"));
+        }
+
+        public void setFuzzyEngine()
+        {
+            fe = new FuzzyEngine();
+            fe.LinguisticVariableCollection.Add(mywaterlevel);
+            fe.LinguisticVariableCollection.Add(mytemp);
+            fe.LinguisticVariableCollection.Add(myheatknob);
+            fe.FuzzyRuleCollection = myrules;
+        }
+
+        public void setCharts()
+        {
+            //FOR TEMPERATURE
+            tempChart.Series[0].Name = "Temperature";
+            tempChart.Series[0].Color = Color.BlueViolet;
+            tempChart.Series[0].ChartType = SeriesChartType.Line;
+            tempChart.Series[0].Points.AddY(temperature);
+            tempChart.ChartAreas[0].AxisY.Minimum = 0;
+            tempChart.ChartAreas[0].AxisY.Maximum = 125;
+
+            //FOR HEAT
+            heatChart.Series[0].Name = "Heat";
+            heatChart.Series[0].Color = Color.OrangeRed;
+            heatChart.Series[0].ChartType = SeriesChartType.Line;
+            heatChart.Series[0].Points.AddY(heat);
+            heatChart.ChartAreas[0].AxisY.Minimum = 0;
+            heatChart.ChartAreas[0].AxisY.Maximum = 10;
+        }
+
+        private void ONButton_Click(object sender, EventArgs e)
+        {
+            waterlevelTextBox.Enabled = false;
+            ONButton.Enabled = false;
+            OFFButton.Enabled = true;
+            try
+            {
+                new Thread(() =>
+                {
+                    while (!exit)
+                    {
+                        if (!ONButton.Enabled)
+                        {
+                            fuzzifyvalues();
+                        }
+                        defuzzifyWaterLevel();
+                        defuzzifyTemp();
+                        defuzzifyHeatKnob();
+                        updateChart();
+                        Thread.Sleep(500);
+                    }
+                }).Start();
+            }
+            finally
+            {
+                exit = false;
+            }
+        }
+
+        private void OFFButton_Click(object sender, EventArgs e)
+        {
+            ONButton.Enabled = true;
+            OFFButton.Enabled = false;
+        }
+
+        public void fuzzifyvalues()
+        {
+            //FOR WATER LEVEL
+            water = double.Parse(waterlevelTextBox.Text);
+            mywaterlevel.InputValue = water;
+            mywaterlevel.Fuzzify("LOW");
+
+            //FOR TEMPERATURE
+            mytemp.InputValue = temperature;
+            mytemp.Fuzzify("VLOW");
+
+            //FOR HEAT KNOB
+            myheatknob.InputValue = heat;
+            myheatknob.Fuzzify("V_LITTLE");
+        }
+
+        public void defuzzifyWaterLevel()
+        {
+            waterlevelDataLabel.Invoke((MethodInvoker)(() =>
+            {
+                waterlevelDataLabel.Text = "Level: \n" + water.ToString("f2");
+            }));
+        }
+
+        public void defuzzifyTemp()
+        {
+            if (!ONButton.Enabled)
+            {
+                if (temperature < targetTemperature) temperature += heat;
+                else temperature -= heat;
+            }
+
+            if (!OFFButton.Enabled)
+            {
+                temperature -= 5;
+                if (temperature < 0) temperature = 0;
+            }
+
+            tempDataLabel.Invoke((MethodInvoker)(() =>
+            {
+                tempDataLabel.Text = "Temperature: \n" + temperature.ToString("f2") + " °C";
+            }));
+        }
+
+        private void waterlevelTextBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (double.Parse(waterlevelTextBox.Text) < 0 || double.Parse(waterlevelTextBox.Text) > 10)
+            {
+                MessageBox.Show("INPUT MUST RANGE WITHIN 0 TO 10");
+                waterlevelTextBox.Text = "";
+                ONButton.Enabled = false;
+                return;
+            }
+            ONButton.Enabled = true;
+        }
+
+        public void defuzzifyHeatKnob()
+        {
+            if (!ONButton.Enabled)
+            {
+                fe.Consequent = "HEAT_KNOB";
+                heat = fe.Defuzzify();
+            }
+            if (!OFFButton.Enabled)
+            {
+                heat--;
+                if (heat < 0) heat = 0;
+            }
+
+            heatknobDataLabel.Invoke((MethodInvoker)(() =>
+            {
+                heatknobDataLabel.Text = "Heat Knob: \n" + heat.ToString("f2") + " kW";
+            }));
+        }
+
+        public void updateChart()
+        {
+            tempChart.Invoke((MethodInvoker)(() =>
+            {
+                if (tempChart.Series[0].Points.Count > 25) tempChart.Series[0].Points.RemoveAt(0);
+                tempChart.Series[0].Points.AddY(temperature);
+                tempChart.ChartAreas[0].AxisX.Minimum = tempChart.Series[0].Points[0].XValue;
+                tempChart.ChartAreas[0].AxisX.Maximum = 25;
+            }));
+
+            heatChart.Invoke((MethodInvoker)(() =>
+            {
+                if (heatChart.Series[0].Points.Count > 25) heatChart.Series[0].Points.RemoveAt(0);
+                heatChart.Series[0].Points.AddY(heat);
+                heatChart.ChartAreas[0].AxisX.Minimum = tempChart.Series[0].Points[0].XValue;
+                heatChart.ChartAreas[0].AxisX.Maximum = 25;
+            }));
+        }
+        private void RESETButton_Click(object sender, EventArgs e)
+        {
+            exit = true;
+            ONButton.Enabled = true;
+            OFFButton.Enabled = false;
+            waterlevelTextBox.Enabled = true;
+            waterlevelTextBox.Text = "0";
+            temperature = 0;
+            heat = 0;
+            tempChart.Series[0].Points.Clear();
+            heatChart.Series[0].Points.Clear();
+            setCharts();
         }
     }
 }
